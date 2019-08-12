eea5054caa1f47234d4ece3f56560c2bf21b5ba0
JohnHarlepas <harlepas@gmail.com>
2019-08-12 08:19:47 -0400

    fixed centering issue for video

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Route, BrowserRouter } from "react-router-dom";
+import ReactGA from 'react-ga'
 
 import "./index.css";
 import App from "./App";
+
+
+ReactGA.initialize('UA-145077240-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
+
 
 ReactDOM.render(
     <BrowserRouter basename={"/"}>
